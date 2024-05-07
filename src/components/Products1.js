302589efa86984ba364113277f0e7b946f407828
Hemarajsgithub <hemaraj2708@gmail.com>
2024-05-07 14:42:45 +0530

    Router Files

--- src/components/Products1.js
+++ src/components/Products1.js
@@ -6,51 +6,76 @@ import { Link } from "react-router-dom";
 const Products1 = () => {
   // JSON - Javascript Object Notation - {  }
 
-  let [products, setProducts] = useState(null);
+  let [products, setProducts] = useState([]);
+  let[error,setError] = useState( null );
+  let[isLoading, setIsLoading] = useState( true )
 
-  useEffect(() => {
+  useEffect(() => {  // Mounting
     let fetchData = async () => {
-      let res = await fetch("http://localhost:4000/products", {
-        method : "GET"
-      });
-      
-      let data = await res.json();
-      setProducts(data);
+      try{
+        let res = await fetch("https://fakestoreapi.com/products")
+
+        if( !res.ok ){
+            throw  new Error ( "Network Issues..." )
+        }
+
+        let data = await res.json();
+        setProducts(data);
+
+      }
+      catch(err){
+        setError( err.message ); 
+      }
+      finally{
+        setIsLoading( false )
+      }
     };
     fetchData();
   }, []);
 
+  if( isLoading ){
+    return <div> Loading..... </div>
+  }
+
+  if( error){
+    return <div> Error : {error} </div> 
+  }
 
   return (
-    <div className="container">
-      <div className="d-flex justify-content-between">
-        <Button as={Link} to="/" variant="primary">
-          Back to Home
-        </Button>
-        <h1> Total Products : {products.length} </h1>
+      <div className="container">
+        <div className="d-flex justify-content-between">
+          <Button as={Link} to="/" variant="primary">
+            Back to Home
+          </Button>
+          <h1> Total Products : {products.length} </h1>
+        </div>
+        <section className="row">
+          {products !== null &&
+            products.map((product) => (
+              <Card
+                style={{ width: "18rem", height: "auto" }}
+                className="m-3 d-grid align-item-between"
+                key={product.id}
+              >
+                <article>
+                  <Card.Img variant="top" src={product.image} />
+                  <Card.Body>
+                    <Card.Title>{product.title}</Card.Title>
+                    <Card.Text> {product.category}</Card.Text>
+                    <Card.Text> {product.description}</Card.Text>
+                    <Card.Text> {product.price}</Card.Text>
+                  </Card.Body>
+                </article>
+                <hr />
+                <center>
+                  <Button variant="primary">Add to Card</Button>
+                </center>
+              </Card>
+            ))}
+        </section>
       </div>
-      <section className="row">
-        { products !== null &&
-          products.map((product) => (
-            <Card style={{ width: "18rem", height:"auto" }} className="m-3 d-grid align-item-between" key={product.id}>
-              <article>
-                <Card.Img variant="top" src={product.images[0]} />
-                <Card.Body>
-                  <Card.Title>{product.title}</Card.Title>
-                  <Card.Text> {product.brand}</Card.Text>
-                  <Card.Text> {product.description}</Card.Text>
-                  <Card.Text> {product.price}</Card.Text>
-                </Card.Body>
-              </article>
-              <hr />
-              <center>
-                <Button variant="primary">Add to Card</Button>
-              </center>
-            </Card>
-          ))}
-      </section>
-    </div>
-  );
+    );
+
 };
 
 export default Products1;
